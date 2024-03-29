631063358753d4c078b0c48ab9e1db1def9e68af
3alyef <3alyef.dev@gmail.com>
2024-03-29 20:24:08 -0300

    up

--- src/DataBase/DataBase.ts
+++ src/DataBase/DataBase.ts
@@ -1,10 +1,14 @@
 import mongoose, { Document, Model } from "mongoose";
 async function authenticate() {
-    const DB_HOST: string = process.env.DB_HOST || "need host";
+    const db_local: any = process.env.DB_HOST ;
 
+    const db_inCloud: string = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mspostel.ozftuyn.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_INCLOUD_NAME}`;
+    
+    const url_database: string = db_local || db_inCloud;
     try {
         // Conectar ao banco de dados usando a URL de conexão fornecida
-        await mongoose.connect(`${DB_HOST}`);
+
+        await mongoose.connect(`${url_database}`);
         console.log('Conexão bem-sucedida com o banco de dados MongoDB.');
     } catch (error) {
         console.error('Erro ao conectar ao banco de dados MongoDB:', error);
