146805890b7b8c8e853207a3c6cd76bd36b75357
Alef A. Araujo <3alyef.dev@gmail.com>
2024-09-24 23:03:00 -0300

    redefinicao de api para nestjs

--- tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup"
-export default defineConfig({
-  // entry: ["src", "!src/views/**/*.html"]
-	external: ['src/views/**/*.html'], 
-  loader: {
-    '.html': 'file', 
-  },
-	ignoreWatch: ['src/views/**/*.html']
-});
